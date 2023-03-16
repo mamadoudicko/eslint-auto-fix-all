05016c0a62c15d1bc8b0ff9689b043d8f3869929
Mamadou DICKO <mamadoudicko100@gmail.com>
2023-03-16 12:49:57 +0100

    ✨ add operations example

---
+++ src/1operations.ts
@@ -0,0 +1,3 @@
+export const add = (a: number, b: number) => a + b;
+export const mul = (a: number, b: number) => a * b;
+export const div = (a: number, b: number) => a / b;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <div>Hello</div>;
-}
+import { sub } from "./2operations";
+import { add } from "./1operations";
+import { mul } from "./1operations";
+import { div } from "./1operations"; // unused
 
-export default App;
+export const App = () => {
+  const a = 1;
+  const b = 2;
+  return (
+    <>
+      <div>{add(a, b)}</div>
+      <div>{mul(a, b)}</div>
+      <div>{sub(a, b)}</div>
+    </>
+  );
+};
+
+//@mamadoudicko_
